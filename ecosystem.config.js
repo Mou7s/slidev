b553f1bba8981247da195b2a413d1d7258ba92e4
mou7s <mou7s@icloud.com>
2025-11-27 18:03:30 +0800

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: 'slidev',
+      name: "slidev-preview",
       cwd: __dirname,
-      script: 'pnpm',
-      args: 'dev -- --host 0.0.0.0 --port 3030',
-      interpreter: 'bash',
+      script: "pnpm",
+      args: "exec slidev slides --host 0.0.0.0 --port 3030",
+      interpreter: "none",
       env: {
-        NODE_ENV: 'development',
+        NODE_ENV: "production",
       },
       autorestart: true,
       max_restarts: 5,
       watch: false,
-      error_file: 'logs/pm2-err.log',
-      out_file: 'logs/pm2-out.log',
+      error_file: "logs/pm2-err.log",
+      out_file: "logs/pm2-out.log",
     },
   ],
 };
