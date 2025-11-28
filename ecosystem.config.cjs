29c4a77d16e68ac86c46d3e804bc85250121328f
mou7s <mou7s@icloud.com>
2025-11-28 11:29:38 +0800

    changed ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,18 +1,21 @@
 module.exports = {
   apps: [
     {
-      name: 'slidev-preview',
+      name: 'slidev',
+      script: 'node',
+      args: './node_modules/@slidev/cli/bin/slidev.js dev --host 0.0.0.0 --port 3030 --open false',
       cwd: __dirname,
-      script: 'pnpm',
-      args: 'exec slidev preview slides.md --host 0.0.0.0 --port 3030',
       env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
         NODE_ENV: 'production',
       },
-      autorestart: true,
-      max_restarts: 5,
       watch: false,
-      error_file: 'logs/pm2-err.log',
-      out_file: 'logs/pm2-out.log',
+      error_file: 'logs/slidev-err.log',
+      out_file: 'logs/slidev-out.log',
+      merge_logs: true,
+      max_memory_restart: '1G',
     },
   ],
 };
